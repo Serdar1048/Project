d2229e5333dbb0ae6e176334726a8b72d5019b84
U220717040 <alikorayurun@posta.mu.edu.tr>
2024-05-09 19:11:21 +0300

    koray2

--- src/kisiselKullanim/Koray.java
+++ src/kisiselKullanim/Koray.java
@@ -3,5 +3,7 @@ package kisiselKullanim;
 public class Koray {
     public static void main(String[] args) {
         System.out.println("DATABASE(KORAY) BURADA");
+
+        System.out.println("aaaaaaaaaaa");
     }
 }
