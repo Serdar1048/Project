33e09ac7392c7c2db3014dcd1fb45512adb1c423
U220717004 <mahmutyusufoz@posta.mu.edu.tr>
2024-05-09 17:54:50 +0300

    ysusf3

--- src/kisiselKullanim/Yusuf.java
+++ src/kisiselKullanim/Yusuf.java
@@ -4,5 +4,6 @@ public class Yusuf {
     public static void main(String[] args) {
         System.out.println("Yusuf");
         System.out.println("yusuf2");
+        System.out.println("ysuuf3");
     }
 }
