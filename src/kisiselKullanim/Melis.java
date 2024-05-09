34b6b7a7f148366a1634f7aa97ab89ecb01a1309
U220717056 <meliskirik@posta.mu.edu.tr>
2024-05-09 13:26:20 +0300

    Melis 2 eklendi.

--- src/kisiselKullanim/Melis.java
+++ src/kisiselKullanim/Melis.java
@@ -2,6 +2,9 @@ package kisiselKullanim;
 
 public class Melis {
     public static void main(String[] args) {
+
         System.out.println("Hello it's melis");
+
+        System.out.println("Melis 2 ");
     }
 }
